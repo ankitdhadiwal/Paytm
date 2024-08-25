40cb5dd511a9ae68669766b44015c60bae299818
ankitdhadiwal <ankit.dhadiwal.885@gmail.com>
2024-08-25 19:26:22 +0530

    ||

---
+++ backend/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/");
+
+const userSchema = mongoose.Schema({
+    username : String,
+    password : String,
+    firstName : String,
+    lastName : String,
+})
+
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+
+const Account = mongoose.model('Account', accountSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+    User,
+    Account,
+};
